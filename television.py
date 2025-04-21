e1f7d5c887a3bc19e523c75e1fd522c75c4d2503
PWilliams1919 <parkerwilliams@unomaha.edu>
2025-04-21 16:36:52 -0500

    Added Television class and associated attributes/methods.

--- television.py
+++ television.py
@@ -5,10 +5,10 @@ class Television:
     MAX_CHANNEL = 3
 
     def __init__(self):
-        self.__status: bool = False
-        self.__muted: bool = False
-        self.__volume: int = Television.MIN_VOLUME
-        self.__channel: int = Television.MIN_CHANNEL
+        self.__status = False
+        self.__muted = False
+        self.__volume = Television.MIN_VOLUME
+        self.__channel = Television.MIN_CHANNEL
 
     def power(self):
         if self.__status:
